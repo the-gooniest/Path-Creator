2300de3cfe0c20441272615bc504b81ef650b309
Devin Miller <devin.miller@unity3d.com>
2020-01-08 11:48:27 -0800

    Fixed distance computation for placing new 3d points
    When placing a new point on a non-closed path, the expected behavior is
    that the point would be placed a distance from the scene camera equal to
    the distance from the scene camera and the last point on the spline.
    However, distance was being computed by comparing the scene camera's
    global coordinates and the end point's local coordinates, meaning the
    computation would only be correct when the parent game object is located
    at the scene origin.
    
    Modified the distance computation to compare distances using the end
    point's global position. Also added a check to pick the "end point"
    based on whether the new point is getting appended or prepended.
    
    Tested manually in scene with a PathCreator component.

--- com.unity.pathcreator/Assets/PathCreator/Core/Editor/PathEditor.cs
+++ com.unity.pathcreator/Assets/PathCreator/Core/Editor/PathEditor.cs
@@ -373,18 +373,24 @@ namespace PathCreationEditor {
                     // If path is not a closed loop, add new point on to the end of the path
                     else if (!bezierPath.IsClosed)
                     {
-                        var endPointLocal = bezierPath[bezierPath.NumPoints - 1];
-                        var endPointGlobal = MathUtility.TransformPoint(endPointLocal, creator.transform, bezierPath.Space);
+                        // If control/command are held down, the point gets pre-pended, so we want to check distance
+                        // to the endpoint we are adding to
+                        var pointIdx = e.control || e.command ? 0 : bezierPath.NumPoints - 1;
                         // insert new point at same dst from scene camera as the point that comes before it (for a 3d path)
-                        var dstCamToEndpoint = (Camera.current.transform.position - endPointGlobal).magnitude;
-                        Vector3 newPathPoint = MouseUtility.GetMouseWorldPosition (bezierPath.Space, dstCamToEndpoint);
-                        newPathPoint = MathUtility.InverseTransformPoint (newPathPoint, creator.transform, bezierPath.Space);
+                        var endPointLocal = bezierPath[pointIdx];
+                        var endPointGlobal =
+                            MathUtility.TransformPoint(endPointLocal, creator.transform, bezierPath.Space);
+                        var distanceCameraToEndpoint = (Camera.current.transform.position - endPointGlobal).magnitude;
+                        var newPointGlobal = 
+                            MouseUtility.GetMouseWorldPosition (bezierPath.Space, distanceCameraToEndpoint);
+                        var newPointLocal = 
+                            MathUtility.InverseTransformPoint (newPointGlobal, creator.transform, bezierPath.Space);
 
                         Undo.RecordObject (creator, "Add segment");
                         if (e.control || e.command) {
-                            bezierPath.AddSegmentToStart (newPathPoint);
+                            bezierPath.AddSegmentToStart (newPointLocal);
                         } else {
-                            bezierPath.AddSegmentToEnd (newPathPoint);
+                            bezierPath.AddSegmentToEnd (newPointLocal);
                         }
 
                     }
